e299a9aef3923e04ba8fc27b91ae0dba99543410
Chris Toshok <toshok@gmail.com>
2012-06-21 18:28:27 -0700

    add the context/this args to _ejs_script

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
-extern int* _ejs_script();
+#include "ejs.h"
+#include "object.h"
 
-int main()
+extern int* _ejs_script(void* __ejs_context, void *__ejs_this);
+
+int
+main(int argc, char* argv)
 {
-  _ejs_script();
+  _ejs_script(NULL, _ejs_object_new(NULL));
 }
