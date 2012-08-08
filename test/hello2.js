4b8909ecce194cd6fce29584691077e4dda12f35
Chris Toshok <toshok@gmail.com>
2012-08-08 11:10:39 -0700

    add more different hello world tests

--- test/hello2.js
+++ test/hello2.js
@@ -1,9 +1,5 @@
-function log(i) {
-  var x = 5;
-  function really_log(str) {
-    print (str + " " + x);
-  }
-  really_log("hi");
+function print_hello() {
+  print ("hello world");
 }
 
-log(0);
+print_hello();
