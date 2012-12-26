8e4c05515842fbeb321c106492c1d1de4da70e6d
Chris Toshok <toshok@gmail.com>
2012-12-26 05:19:28 -0800

    add macros for internal lists

--- runtime/ejs-types.h
+++ runtime/ejs-types.h
@@ -15,4 +15,36 @@ typedef unsigned long long uint64_t;
 # define EJS_UNLIKELY(x) (x)
 #endif
 
+#define EJS_LIST_HEADER(t) \
+  t* prev; \
+  t* next
+#define EJS_LIST_INIT(v) do { \
+    v->prev = v->next = NULL; \
+  } while(0)
+
+#define EJS_LIST_ATTACH(v,l) do { \
+  v->next = l;			  \
+  if (l) l->prev = v;		  \
+  l = v;			  \
+  } while (0)
+
+#define EJS_LIST_DEATTACH(v,l) do {		\
+  if (v->next) v->next->prev = v->prev;		\
+  if (v->prev) v->prev->next = v->next;		\
+  if (l == v) l == v->next;			\
+  EJS_LIST-INIT(v);				\
+  while (0)
+
+
+#define EJS_SLIST_HEADER(t) \
+  t* next
+#define EJS_SLIST_INIT(v) do {			\
+    v->next = NULL;				\
+  } while(0)
+
+#define EJS_SLIST_ATTACH(v,l) do { \
+  v->next = l;			  \
+  l = v;			  \
+  } while (0)
+
 #endif /* _ejs_types_h */
