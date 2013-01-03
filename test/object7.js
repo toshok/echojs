aa174bf05781a2481fb4b2f43037c1c456be6ef4
Chris Toshok <toshok@gmail.com>
2013-01-02 16:11:11 -0800

    add some property descriptor tests

---
+++ test/object7.js
@@ -0,0 +1,11 @@
+if (typeof(console) == "object") print = console.log;
+
+var a = {};
+
+Object.defineProperty (a, "c", { configurable: true, value: 10 });
+
+var desc = Object.getOwnPropertyDescriptor(a, "c");
+print (desc.value);
+print (desc.configurable);
+print (desc.writable);
+print (desc.enumerable);
---
+++ test/object8.js
@@ -0,0 +1,9 @@
+if (typeof(console) == "object") print = console.log;
+
+var a = {b : 15};
+
+var desc = Object.getOwnPropertyDescriptor(a, "b");
+print (desc.value);
+print (desc.configurable);
+print (desc.writable);
+print (desc.enumerable);
