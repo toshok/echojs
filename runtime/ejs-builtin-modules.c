5a8b62f2bacf722e5f26484c0dae29bc52437266
Chris Toshok <toshok@gmail.com>
2013-01-21 10:35:49 -0800

    implement (poorly, but enough for our driver's purposes):   path.dirname   path.resolve
      fs.createWriteStream (as well as writeStream.write + writeStream.end)
    
      child_process.spawn
    
    in ejs, spawn is synchronous, so we use __ejs? in the driver code to run code which
    assumes that.

--- runtime/ejs-builtin-modules.c
+++ runtime/ejs-builtin-modules.c
@@ -8,9 +8,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
+#include "ejs-ops.h"
 #include "ejs-value.h"
+#include "ejs-array.h"
 #include "ejs-function.h"
 #include "ejs-builtin-modules.h"
 
@@ -20,6 +24,14 @@
 ///
 
 static ejsval
+_ejs_path_dirname (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
+{
+    ejsval path = args[0];
+    // FIXME node's implementation allows a second arg to strip the extension, but the compiler doesn't use it.
+    return _ejs_string_new_utf8(dirname (EJSVAL_TO_FLAT_STRING(path)));
+}
+
+static ejsval
 _ejs_path_basename (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
 {
     ejsval path = args[0];
@@ -27,14 +39,27 @@ _ejs_path_basename (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
     return _ejs_string_new_utf8(basename (EJSVAL_TO_FLAT_STRING(path)));
 }
 
+static ejsval
+_ejs_path_resolve (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
+{
+    // FIXME node's implementation is a lot more flexible.  we just combine the paths with a / between them.
+    ejsval from = args[0];
+    ejsval to = args[1];
+    return _ejs_string_concat (from, _ejs_string_concat (_ejs_string_new_utf8("/"), to));
+}
+
 ejsval
 _ejs_path_module_func (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
 {
+    START_SHADOW_STACK_FRAME;
+
     ejsval exports = args[0];
 
-    ejsval name = _ejs_string_new_utf8("basename");
+    EJS_INSTALL_FUNCTION(exports, "dirname", _ejs_path_dirname);
+    EJS_INSTALL_FUNCTION(exports, "basename", _ejs_path_basename);
+    EJS_INSTALL_FUNCTION(exports, "resolve", _ejs_path_resolve);
 
-    _ejs_object_setprop (exports, name, _ejs_function_new (_ejs_null, name, _ejs_path_basename));
+    END_SHADOW_STACK_FRAME;
 
     return _ejs_undefined;
 }
@@ -62,17 +87,133 @@ _ejs_fs_readFileSync (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
 }
 
 ejsval
-_ejs_fs_module_func (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
+_ejs_stream_write (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
 {
-    ejsval exports = args[0];
+    ejsval to_write = ToString(args[0]);
+    ejsval internal_fd = _ejs_object_getprop_utf8 (_this, "%internal_fd");
+    int fd = ToInteger(internal_fd);
 
-    _ejs_object_setprop_utf8 (exports, "readFileSync", _ejs_function_new_utf8 (_ejs_null, "readFileSync", _ejs_fs_readFileSync));
+    int remaining = EJSVAL_TO_STRLEN(to_write);
+    int offset = 0;
+    char *buf = EJSVAL_TO_FLAT_STRING(to_write);
+    
+    do {
+        int num_written = write (fd, buf + offset, remaining);
+        if (num_written == -1) {
+            if (errno == EINTR)
+                continue;
+            perror ("write");
+            return _ejs_undefined;
+        }
+        remaining -= num_written;
+        offset += num_written;
+    } while (remaining > 0);
 
     return _ejs_undefined;
 }
 
 ejsval
+_ejs_stream_end (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
+{
+    ejsval internal_fd = _ejs_object_getprop_utf8 (_this, "%internal_fd");
+    close (ToInteger(internal_fd));
+    return _ejs_undefined;
+}
+
+ejsval
+_ejs_fs_createWriteStream (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
+{
+    START_SHADOW_STACK_FRAME;
+
+    ejsval stream = _ejs_object_create(_ejs_null);
+
+    EJS_INSTALL_FUNCTION (stream, "write", _ejs_stream_write);
+    EJS_INSTALL_FUNCTION (stream, "end", _ejs_stream_end);
+
+    ejsval path = args[0];
+
+    int fd = open (EJSVAL_TO_FLAT_STRING(path), O_CREAT | O_TRUNC | O_WRONLY, 0777);
+    if (fd == -1) {
+        perror ("open");
+        printf ("we should totally throw an exception here\n");
+        return _ejs_undefined;
+    }
+
+    _ejs_object_setprop_utf8 (stream, "%internal_fd", NUMBER_TO_EJSVAL(fd));
+
+    END_SHADOW_STACK_FRAME;
+    return stream;
+}
+
+ejsval
+_ejs_fs_module_func (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
+{
+    START_SHADOW_STACK_FRAME;
+
+    ejsval exports = args[0];
+
+    EJS_INSTALL_FUNCTION(exports, "readFileSync", _ejs_fs_readFileSync);
+    EJS_INSTALL_FUNCTION(exports, "createWriteStream", _ejs_fs_createWriteStream);
+
+    END_SHADOW_STACK_FRAME;
+
+    return _ejs_undefined;
+}
+
+
+static ejsval
+_ejs_child_process_spawn (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
+{
+    char* argv0 = EJSVAL_TO_FLAT_STRING(args[0]);
+    EJSArray* argv_rest = (EJSArray*)EJSVAL_TO_OBJECT(args[1]);
+
+    char **argv = (char**)calloc(sizeof(char*), EJSARRAY_LEN(argv_rest) + 2);
+    argv[0] = argv0;
+    for (int i = 0; i < EJSARRAY_LEN(argv_rest); i ++)
+        argv[1+i] = EJSVAL_TO_FLAT_STRING(ToString(EJSARRAY_ELEMENTS(argv_rest)[i]));
+
+    printf ("in _ejs_child_process_spawn:\n");
+    for (int i = 0; i < EJSARRAY_LEN(argv_rest) + 1; i++)
+        printf ("  argv[%d] = %s\n", i, argv[i]);
+
+    pid_t pid;
+    switch (pid = fork()) {
+    case -1: /* error */
+        perror("fork");
+        printf ("we should totally throw an exception here\n");
+        break;
+    case 0:  /* child */
+        execvp (argv0, argv);
+        perror("execv");
+        EJS_NOT_REACHED();
+        break;
+    default: /* parent */ {
+        int stat;
+        int wait_rv;
+        do {
+            wait_rv = waitpid(pid, &stat, 0);
+        } while (wait_rv == -1 && errno == EINTR);
+
+        if (wait_rv != pid) {
+            perror ("waitpid");
+            printf ("we should totally throw an exception here\n");
+        }
+        break;
+    }
+    }
+    return _ejs_undefined;
+}
+
+
+ejsval
 _ejs_child_process_module_func (ejsval env, ejsval _this, uint32_t argc, ejsval* args)
 {
+    START_SHADOW_STACK_FRAME;
+    ejsval exports = args[0];
+
+    EJS_INSTALL_FUNCTION(exports, "spawn", _ejs_child_process_spawn);
+
+    END_SHADOW_STACK_FRAME;
+
     return _ejs_undefined;
 }
