15b4dd50e0c2c5651ee702ab16ef28b1985acdff
Chris Toshok <toshok@gmail.com>
2013-01-24 08:23:31 -0800

    i love it when becoming more spec compliant fixes tests.  sieve1 seems to work fine now.  change the way results/expected-failures work to keep the presence of 'array12' in EXPECTED_FAILURES from also causing 'array1' to be an expected failure

--- test/array12.js
+++ test/array12.js
@@ -4,3 +4,17 @@ b = a.splice(1);
 console.log (b[0]);
 console.log (a[0]);
 
+console.log();
+console.log(a.length);
+a.splice(1, 0, 6);
+console.log(a.length);
+console.log (a[0]);
+console.log (a[1]);
+
+console.log();
+var a = [1,2,3,4,5];
+a.splice(1);
+a.splice(1, 0, 6);
+console.log (a[0]);
+console.log (a[1]);
+console.log (a[2]);
