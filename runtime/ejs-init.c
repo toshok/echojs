c0fafd158c45622183cdf17af662bd2be6c517c8
Chris Toshok <toshok@gmail.com>
2013-01-27 10:53:28 -0800

    we already have an atom for this.  we should use it.

--- runtime/ejs-init.c
+++ runtime/ejs-init.c
@@ -55,7 +55,7 @@ _ejs_init(int argc, char** argv)
 
     _ejs_object_setprop (_ejs_global, _ejs_atom_undefined, _ejs_undefined);
     _ejs_object_setprop (_ejs_global, _ejs_atom_NaN, _ejs_nan);
-    _ejs_object_setprop_utf8 (_ejs_global, "__ejs", _ejs_ejs_global);
+    _ejs_object_setprop (_ejs_global, _ejs_atom___ejs, _ejs_ejs_global);
 
     _ejs_object_init_proto();
 
