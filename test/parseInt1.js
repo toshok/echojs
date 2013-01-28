2073558c50f845509b4f5943ddb306729660044b
Chris Toshok <toshok@gmail.com>
2013-01-28 07:28:44 -0800

    add a parseInt test

---
+++ test/parseInt1.js
@@ -0,0 +1,9 @@
+
+console.log (parseInt(10));
+console.log (parseInt("10"));
+
+console.log (parseInt(10, 8));
+console.log (parseInt("10", 8));
+
+console.log (parseInt("0xaf"));
+console.log (parseInt("0xaf", 16));
