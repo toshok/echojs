dac2833275803ba77d3cc1e0007a64bbfa1dfdda
Chris Toshok <toshok@gmail.com>
2013-02-01 21:11:53 -0800

    more switch tests

---
+++ test/switch2.js
@@ -0,0 +1,5 @@
+
+switch (0) {
+  case 0: console.log ("hi");
+  case 1: console.log ("bye");
+}
---
+++ test/switch3.js
@@ -0,0 +1,5 @@
+
+switch (0) {
+  case 0: console.log ("hi"); break;
+  case 1: console.log ("bye"); break;
+}
