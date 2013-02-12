100ba9b7ef112c8ab48caa889252acf289d17c67
Chris Toshok <toshok@gmail.com>
2013-02-13 08:40:31 +0900

    we need to specify NODE_PATH when running ejs this way

--- test/mozilla-tests/ejs-driver.sh
+++ test/mozilla-tests/ejs-driver.sh
@@ -2,6 +2,8 @@
 
 rm -f $2.exe
 
-../../ejs $@
+TOPDIR=../..
+
+NODE_PATH="$TOPDIR/node-llvm/build/Release:$TOPDIR/lib/coffee:$TOPDIR/lib:$TOPDIR/esprima:$TOPDIR/escodegen" $TOPDIR/ejs $@
 
 exec $2.exe
