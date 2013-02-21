d2bcb83ab3d5c9dfda5fc5b6d2986a81036d8749
Chris Toshok <toshok@gmail.com>
2013-02-21 09:58:35 -0800

    add some more tests for inner/outer loops with try/finally blocks

---
+++ test/exc14.js
@@ -0,0 +1,17 @@
+
+outer: while (true) {
+  try {
+    inner: while (true) {
+      try {
+        break outer;
+      }
+      finally {
+        console.log ("inner finally");
+      }
+    }
+  }
+  finally {
+    console.log ("outer finally");
+  }
+}
+console.log ("done");
---
+++ test/exc15.js
@@ -0,0 +1,17 @@
+
+outer: while (true) {
+  try {
+    inner: while (true) {
+      try {
+        break outer;
+      }
+      catch (e) {
+        console.log ("inner catch");
+      }
+    }
+  }
+  finally {
+    console.log ("outer finally");
+  }
+}
+console.log ("done");
