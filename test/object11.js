122324cbe1dc4ccad02e290a517d04b8e625ec23
Chris Toshok <toshok@gmail.com>
2013-03-31 17:27:37 -0700

    add test for overwriting accessor properties.

---
+++ test/object11.js
@@ -0,0 +1,13 @@
+if (typeof(console) == "object") print = console.log;
+
+var a = Object.create(null);
+
+Object.defineProperty (a, "b", { get: function() { return 15; }, set: function(v) { print ("1: setting value to " + v); }, configurable: true });
+
+a.b = 10;
+
+Object.defineProperty (a, "b", { get: function() { return 5; }, set: function(v) { print ("2: setting value to " + v); }, configurable: true });
+
+a.b = 10;
+
+console.log (a.b);
