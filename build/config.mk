9d720d35ff4d59b011e05bc6db40a8755d109eaa
Chris Toshok <toshok@gmail.com>
2013-04-02 17:43:32 -0700

    make the /Developer prefix configurable, as well as the IOS SDK version to compile against.

--- build/config.mk
+++ build/config.mk
@@ -27,21 +27,24 @@ CP=cp
 
 CFLAGS=-g -O0 -Wall -I. -Wno-unused-function
 
-MIN_IOS_VERSION=4.3
+MIN_IOS_VERSION=5.0
+
+DEVELOPER_ROOT?=/Developer
+IOS_SDK_VERSION?=5.0
 
 IOSSIM_ARCH=-arch i386
 IOSSIM_ARCH_FLAGS=
 IOSSIM_TRIPLE=i386-apple-darwin
-IOSSIM_ROOT=/Developer/Platforms/iPhoneSimulator.platform/Developer
+IOSSIM_ROOT=$(DEVELOPER_ROOT)/Platforms/iPhoneSimulator.platform/Developer
 IOSSIM_BIN=$(IOSSIM_ROOT)/usr/bin
-IOSSIM_SYSROOT=$(IOSSIM_ROOT)/SDKs/iPhoneSimulator5.0.sdk
+IOSSIM_SYSROOT=$(IOSSIM_ROOT)/SDKs/iPhoneSimulator$(IOS_SDK_VERSION).sdk
 
 IOSDEV_ARCH=-arch armv7
 IOSDEV_ARCH_FLAGS=-mthumb
 IOSDEV_TRIPLE=armv7-apple-darwin
-IOSDEV_ROOT=/Developer/Platforms/iPhoneOS.platform/Developer
+IOSDEV_ROOT=$(DEVELOPER_ROOT)/Platforms/iPhoneOS.platform/Developer
 IOSDEV_BIN=$(IOSDEV_ROOT)/usr/bin
-IOSDEV_SYSROOT=$(IOSDEV_ROOT)/SDKs/iPhoneOS5.0.sdk 
+IOSDEV_SYSROOT=$(IOSDEV_ROOT)/SDKs/iPhoneOS$(IOS_SDK_VERSION).sdk 
 
 IOSSIM_CFLAGS=$(IOSSIM_ARCH) $(IOSSIM_ARCH_FLAGS) $(CFLAGS) -DIOS=1 -isysroot $(IOSSIM_SYSROOT) -miphoneos-version-min=$(MIN_IOS_VERSION)
 IOSDEV_CFLAGS=$(IOSDEV_ARCH) $(IOSDEV_ARCH_FLAGS) $(CFLAGS) -DIOS=1 -isysroot $(IOSDEV_SYSROOT) -miphoneos-version-min=$(MIN_IOS_VERSION)
