aedd22d89a2cd015873f7c04e0b597e57700bf71
Chris Toshok <toshok@gmail.com>
2013-07-24 08:50:22 +0900

    another regexp test

---
+++ test/regexp4.js
@@ -0,0 +1,9 @@
+
+var str = "a1a2a3a4a5";
+str = str.replace(/\d/g, "");
+console.log("'" + str + "'");
+
+
+var str = "a1a2a3a4a5";
+str = str.replace(new RegExp("\\d", "g"), "");
+console.log("'" + str + "'");
