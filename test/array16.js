0ed245a88028ab0ccacb2a2625c2485036103ee8
Chris Toshok <toshok@gmail.com>
2013-07-29 08:55:56 +0900

    remove duplicate line, and we aren't testing exceptions here so don't deref null.

--- test/array16.js
+++ test/array16.js
@@ -1,5 +1,3 @@
 console.log (Object.prototype.toString.call([].__proto__));
 console.log (Object.prototype.toString.call([].__proto__.__proto__));
 console.log (Object.prototype.toString.call([].__proto__.__proto__.__proto__));
-console.log (Object.prototype.toString.call([].__proto__.__proto__.__proto__));
-console.log (Object.prototype.toString.call([].__proto__.__proto__.__proto__.__proto__));
