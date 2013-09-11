997175519e1d04eed6b40e2ebae72c49edc42eca
Chris Toshok <toshok@gmail.com>
2013-09-11 09:04:16 -0700

    some === tests

---
+++ test/stricteq1.js
@@ -0,0 +1,8 @@
+
+console.log (("hi " + "there") === "hi there");
+
+var hi = "hi ";
+var there = "there";
+
+console.log ((hi + there) === "hi there");
+console.log ((hi + there) === (hi + there));
