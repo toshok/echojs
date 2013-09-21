108a09d94b69fa8c30ea47303f9c1d00dd39db30
Chris Toshok <toshok@gmail.com>
2013-09-21 09:49:19 -0700

    a couple more iife tests

---
+++ test/iife5.js
@@ -0,0 +1,9 @@
+var x = "hello world";
+
+console.log (x);
+(function() {
+  var x = "goodbye world";
+  console.log (x);
+  return x;
+})();
+console.log (x);
---
+++ test/iife6.js
@@ -0,0 +1,6 @@
+var i = 10;
+var total;
+total = (function() {
+     return i;
+})();
+console.log (total);
