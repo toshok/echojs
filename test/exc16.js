fd0656445505480f7b63846c0898506bdebba40f
Chris Toshok <toshok@gmail.com>
2013-09-21 21:49:10 -0700

    add regression test for exception bug

---
+++ test/exc16.js
@@ -0,0 +1,25 @@
+
+function foo() {
+  try {
+    return 5;
+  }
+  catch (e) {
+  }
+  finally {
+    console.log ("hello");
+  }
+}
+
+function bar() {
+  try {
+    return 10;
+  }
+  catch (e) {
+  }
+  finally {
+    console.log ("world");
+  }
+}
+
+foo();
+bar();
