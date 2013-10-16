eaa59a87110c52e8e31e4e3d7d777cbc65ec5655
Chris Toshok <toshok@gmail.com>
2013-10-16 11:28:28 -0700

    another arrow function test

---
+++ test/arrow2.js
@@ -0,0 +1,18 @@
+
+function Multiplier(factor) {
+  this.factor = factor;
+}
+Multiplier.prototype.multiply = function(x) {
+  if (Array.isArray(x)) {
+	return x.map ( (el) => el * this.factor );
+  }
+  else {
+	let mult = (x) => { return x * this.factor };
+	return mult (x);
+  }
+}
+
+let mult = new Multiplier (10);
+
+console.log (mult.multiply([1,2,3]));
+console.log (mult.multiply(1));
