702eccb1f2e46abcdc0c2fe10732d1b1beed4f6e
Chris Toshok <toshok@gmail.com>
2014-02-06 09:45:47 -0800

    download and install the node-0.10.25 bottle manually

--- ci/install-node.sh
+++ ci/install-node.sh
@@ -1,4 +1,7 @@
-#hash=$( brew versions nodejs 2>/dev/null | grep ^0.8.22 | awk -F" " '{print $4}' )
-#cd $( brew --prefix )
-#git checkout $hash Library/Formula/node.rb
-brew install nodejs
+# travis-ci osx nodes are mountain_lion, not mavericks
+#OSX=mavericks
+OSX=mountain_lion
+/usr/bin/curl '-f#LA Homebrew 0.9.5 (Ruby 1.8.7-358; Mac OS X 10.8.5)' https://downloads.sf.net/project/machomebrew/Bottles/node-0.10.25.$OSX.bottle.tar.gz -C 0 -o node-0.10.25.$OSX.bottle.tar.gz
+tar -C /usr/local/Cellar -xvf node-0.10.25.$OSX.bottle.tar.gz
+rm node-0.10.25.$OSX.bottle.tar.gz
+brew link node
