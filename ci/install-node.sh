9bb731b5812eae814aba8199aff908e9da589ada
Chris Toshok <toshok@gmail.com>
2014-02-06 19:58:30 -0800

    remove the v from tar to cut down on some spew.

--- ci/install-node.sh
+++ ci/install-node.sh
@@ -2,7 +2,7 @@
 #OSX=mavericks
 OSX=mountain_lion
 /usr/bin/curl '-f#LA Homebrew 0.9.5 (Ruby 1.8.7-358; Mac OS X 10.8.5)' https://downloads.sf.net/project/machomebrew/Bottles/node-0.10.25.$OSX.bottle.tar.gz -C 0 -o node-0.10.25.$OSX.bottle.tar.gz
-tar -C /usr/local/Cellar -xvf node-0.10.25.$OSX.bottle.tar.gz
+tar -C /usr/local/Cellar -zxf node-0.10.25.$OSX.bottle.tar.gz
 rm node-0.10.25.$OSX.bottle.tar.gz
 brew link node
 npm install -g node-gyp
