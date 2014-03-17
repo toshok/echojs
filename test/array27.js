553bf63e4851bdf41e9c1a8948f4322bdc15ccf4
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2014-03-17 20:00:04 +0100

    New test for Array.fill()

---
+++ test/array27.js
@@ -0,0 +1,29 @@
+
+var arr = new Array ('hola', 'world', 'lots', 'of', 'fun');
+console.log (arr);
+
+arr.fill ('1');
+console.log (arr);
+
+arr.fill ('2', 2);
+console.log (arr);
+
+arr.fill ('3', 2, 4);
+console.log (arr);
+
+arr.fill ('4', 3, 100);
+console.log (arr);
+
+arr.fill ('5', -3);
+console.log (arr);
+
+arr.fill ('6', -3, -1);
+console.log (arr);
+
+arr.fill ('7', 1, -2);
+console.log (arr);
+
+arr.fill ('bug', -3, 0);
+console.log (arr);
+
+
