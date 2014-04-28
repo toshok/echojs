bb42afffe404fddd9b90801c8ad35e40474e61a1
Chris Toshok <toshok@gmail.com>
2014-04-28 07:56:48 -0700

    add CS arguments.slice test

---
+++ test/arguments5.js
@@ -0,0 +1,14 @@
+var __slice = [].slice;
+
+var foo = function() {
+  var args, n;
+  n = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+
+  console.log (n);
+  console.log (args.length);
+}
+
+
+foo(1);
+foo(1, 2);
+foo(1, 2, 3, 4);
