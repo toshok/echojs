55461543300f06c5290e38eca8867a41d1fe1e6a
Chris Toshok <toshok@gmail.com>
2014-04-28 07:54:31 -0700

    if EJS_LOG_FILE env var is set, use that as the log file path instead of logging to the console

--- runtime/ejs-log.m
+++ runtime/ejs-log.m
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdarg.h>
 
 #include "ejs.h"
@@ -5,14 +6,37 @@
 #include <Foundation/NSObjCRuntime.h>
 #include <Foundation/NSString.h>
 
+static FILE*   log_file;
+static EJSBool check_log_file;
+
+static void
+init_log_file()
+{
+  if (check_log_file)
+    return;
+
+  check_log_file = EJS_TRUE;
+  char* log_file_name = getenv("EJS_LOG_FILE");
+  if (log_file_name)
+    log_file = fopen (log_file_name, "w");
+}
+
 void
 _ejs_log (const char* fmt, ...)
 {
   va_list va;
 
+  init_log_file();
+
   va_start(va, fmt);
 
-  NSLogv ([NSString stringWithUTF8String:fmt], va);
+  if (log_file) {
+    vfprintf (log_file, fmt, va);
+    fflush(log_file);
+  }
+  else {
+    NSLogv ([NSString stringWithUTF8String:fmt], va);
+  }
 
   va_end(va);
 }
@@ -20,5 +44,13 @@ _ejs_log (const char* fmt, ...)
 void
 _ejs_logstr (const char* str)
 {
-  NSLog (@"%s", str);
+  init_log_file();
+
+  if (log_file) {
+    fprintf (log_file, "%s\n", str);
+    fflush(log_file);
+  }
+  else {
+    NSLog (@"%s", str);
+  }
 }
