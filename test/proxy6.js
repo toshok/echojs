9f90d3fa47450b64107c0d9d71f9d3f1c84f944f
Chris Toshok <toshok@gmail.com>
2014-05-13 07:00:45 +0900

    another proxy test

---
+++ test/proxy6.js
@@ -0,0 +1,36 @@
+// from MDN
+
+let products = new Proxy({
+    browsers: ['Internet Explorer', 'Netscape']
+},
+{
+    get: (obj, prop) => {
+	if (prop === 'latestBrowser') {
+	    return obj.browsers[obj.browsers.length - 1];
+	}
+
+	return obj[prop];
+    },
+    set: (obj, prop, value) => {
+	if (prop === 'latestBrowser') {
+	    obj.browsers.push(value);
+	    return;
+	}
+
+	if (typeof value === 'string') {
+	    value = [value];
+	}
+
+	obj[prop] = value;
+    }
+});
+
+console.log(products.browsers);
+
+products.browsers = 'Firefox';
+console.log(products.browsers);
+
+products.latestBrowser = 'Chrome';
+console.log(products.browsers);
+
+console.log(products.latestBrowser);
