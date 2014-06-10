146146b311e529e21d7c8afc4482bb7a9f9fc676
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2014-06-09 21:48:26 -0500

    Add a test case for Array's values() and keys().

---
+++ test/array30.js
@@ -0,0 +1,16 @@
+
+var arr = ["this", "is", "a", "dummy", "array"];
+var iter;
+console.log (arr);
+
+// 1. Go for keys()
+
+var keys = arr.keys();
+while (!(iter = keys.next ()).done)
+    console.log (iter.value);
+
+// 2. Go for values()
+var values = arr.values();
+while (!(iter = values.next ()).done)
+    console.log (iter.value);
+
