9ce48ef9233a56eded7a68434ba75d00ece7cafb
Chris Toshok <toshok@gmail.com>
2014-06-15 19:02:08 -0700

    add a test that interleaves 'then' with 'catch' and throws from a then handler.  fails currently due to lack of _ejs_invoke_closure_catch

---
+++ test/promise2.js
@@ -0,0 +1,10 @@
+
+Promise.resolve(5).then((value) => {
+    console.log(value);
+    throw new Error(value);
+}).catch((err) => {
+    console.log(err);
+    return 8;
+}).then((value) => {
+    console.log(value);
+});
