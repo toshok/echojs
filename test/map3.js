831ce490e5eea9e79c8a40e8b53453c648e3b370
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2014-07-21 00:53:24 +0200

    Add a test for the new iterator support in Map's ctor.

---
+++ test/map3.js
@@ -0,0 +1,9 @@
+
+var arr = [["usa", "hamburger"], ["mx", "taco"], ["it", "pizza"]];
+var map = new Map (arr);
+
+console.log (map.get ("usa"));
+console.log (map.get ("mx"));
+console.log (map.get ("it"));
+console.log (map.get ("de"));
+
