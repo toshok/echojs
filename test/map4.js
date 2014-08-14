571ecd1952264339c32ae2f0011d65482a31bfcb
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2014-08-13 21:25:38 -0700

    Add test for the new map's iterator support.

---
+++ test/map4.js
@@ -0,0 +1,24 @@
+
+var m = new Map ();
+m.set ("one", "uno");
+m.set ("two", "dos");
+m.set ("three", "tres");
+m.set ("four", "cuatro");
+
+var iter;
+
+var keys = m.keys ();
+while ((iter = keys.next ()).done != true)
+    console.log (iter.value);
+
+var values = m.values ();
+while ((iter = values.next ()).done != true)
+    console.log (iter.value);
+
+var entries = m.entries();
+while ((iter = entries.next ()).done != true)
+    console.log (iter.value);
+
+for (let e of m)
+    console.log (e);
+
