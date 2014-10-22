e1042766044c532fd7c8b2a55d8e56b09487fa79
Chris Toshok <toshok@gmail.com>
2014-10-22 08:15:57 -0700

    bump ios version to 8

--- build/config.mk
+++ build/config.mk
@@ -41,10 +41,10 @@ CP=cp
 
 CFLAGS=-g -O0 -Wall -I. -Wno-unused-function -fno-omit-frame-pointer
 
-MIN_IOS_VERSION=7.1
+MIN_IOS_VERSION=8.0
 
 DEVELOPER_ROOT?=/Applications/Xcode.app/Contents/Developer
-IOS_SDK_VERSION?=7.1
+IOS_SDK_VERSION?=8.0
 
 ifeq ($(HOST_CPU),x86-64)
 LINUX_ARCH=-arch x86_64
