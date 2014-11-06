3490525ea419e233ef2079cd4dde65dbe5c602d6
Chris Toshok <toshok@gmail.com>
2014-11-06 12:17:18 -0800

    add guards, emacs/vim modeline, and BEGIN/END_DECLS

--- runtime/ejs-runloop.h
+++ runtime/ejs-runloop.h
@@ -1,5 +1,19 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99 ft=cpp:
+ */
+
+#ifndef _ejs_runloop_h_
+#define _ejs_runloop_h_
+
+EJS_BEGIN_DECLS
+
 typedef void (*Task)(void* data);
 typedef void (*TaskDataDtor)(void* data);
 
 void _ejs_runloop_add_task(Task task, void* data, TaskDataDtor data_dtor);
 void _ejs_runloop_start();
+
+
+EJS_END_DECLS
+
+#endif /* _ejs_array_h */
