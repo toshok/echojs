e61420f19ccfa9d05a52bd60e1a22e84f0e9d870
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2014-12-06 23:47:42 -0600

    Add a sample based on Apple's tutorial of Cocoa.

---
+++ samples/trackmixcode.js
@@ -0,0 +1,59 @@
+
+import { NSObject } from '$pirouette/foundation';
+import { NSWindow, NSApplicationDelegate, NSApplication, NSButton, NSBezelStyle, NSSlider, NSTextField } from '$pirouette/appkit';
+import { outlet } from '$pirouette/objc';
+
+class Track {
+    constructor() {
+        this.volume = 0;
+    }
+}
+
+let AppDelegate = NSObject.extendClass("AppDelegate", () => ({
+
+    didFinishLaunching: function () {
+        let window = this.window;
+        let contentView = window.contentView;
+
+        window.title = "Track Mix";
+        window.frame = { x: 335, y: 390, width: 300, height: 480 };
+
+        let button = NSButton.newWithFrame({ x: 107, y: 17, width: 82, height: 32 });
+        button.title = "Mute";
+        button.bezelStyle = NSBezelStyle.Rounded;
+        button.clicked = () => this.mute();
+
+        let slider = NSSlider.newWithFrame({ x:138, y: 62, width: 21, height: 321 });
+
+        let textField = NSTextField.newWithFrame({ x: 80, y: 398, width: 136, height: 22});
+        textField.alignment = 2;
+
+        contentView.addSubview(button);
+        contentView.addSubview(slider);
+        contentView.addSubview(textField);
+
+        this.button = button;
+        this.slider = slider;
+        this.textField = textField;
+        this.track = new Track();
+        this.updateUserInterface();
+    },
+
+    mute: function () {
+        this.track.volume = 0;
+        this.updateUserInterface();
+    },
+
+    updateUserInterface: function() {
+        let volume = this.track.volume;
+        this.textField.floatValue = volume;
+        this.slider.floatValue = volume;
+    },
+
+    window: outlet(NSWindow)
+}), [
+    NSApplicationDelegate 
+]);
+
+NSApplication.main (process.argv);
+
