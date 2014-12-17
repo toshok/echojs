b576c21fe9484b6a7323697009f146b47cb28e0a
Chris Toshok <toshok@gmail.com>
2014-12-17 09:07:45 -0800

    add es5-compat-table's test for an (anon) class defined in the 'extends' clause.  ugh :)

---
+++ test/class-in-extends.js
@@ -0,0 +1,11 @@
+class B extends class {
+  qux() { return "bar"; }
+} {
+  qux() { return super.qux() + this.corge; }
+}
+var obj = {
+  qux: B.prototype.qux,
+  corge: "ley"
+};
+console.log(obj.qux() === "barley");
+
