945f8f0e3341018e1a2f09c0b2286c5286068518
Chris Toshok <toshok@gmail.com>
2015-01-24 07:14:44 -0800

    add dist back in for making source packages

--- build/rules.mk
+++ build/rules.mk
@@ -1,20 +1,22 @@
 all: all-local all-recurse all-hook
-
 clean: clean-local clean-recurse clean-hook
-
 install: install-local install-recurse install-hook
+dist: dist-local dist-recurse dist-hook
 
 all-local::
 clean-local::
 install-local::
+dist-local::
 
 all-recurse::
 clean-recurse::
 install-recurse::
+dist-recurse::
 
 all-hook::
 clean-hook::
 install-hook::
+dist-hook::
 
 RECURSE_INTO_SUBDIRS= \
 	@target=`echo $@ | sed -e s/-recurse//`; \
@@ -32,8 +34,12 @@ clean-recurse::
 
 install-recurse::
 	$(RECURSE_INTO_SUBDIRS)
+
+dist-recurse::
+	$(RECURSE_INTO_SUBDIRS)
 endif
 
 .PHONY: all all-recurse all-hook
 .PHONY: clean clean-recurse clean-hook
 .PHONY: install install-recurse install-hook
+.PHONY: dist dist-recurse dist-hook
