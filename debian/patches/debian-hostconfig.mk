c08848d2a5477688a59d409b13093f08a61ae767
Chris Toshok <toshok@gmail.com>
2015-01-23 16:09:58 -0800

    add a host-config.mk for the debian build so dpkg-buildpackage doesn't whine

---
+++ debian/patches/debian-hostconfig.mk
@@ -0,0 +1,9 @@
+Description: host-config.mk for debian build
+--- /dev/null
++++ echo-js-0.0.0alpha2/build/host-config.mk
+@@ -0,0 +1,5 @@
++HOST_TRIPLE:=x86_64-unknown-linux-gnu
++HOST_CPU:=x86_64
++HOST_VENDOR:=unknown
++HOST_OS:=linux
++
