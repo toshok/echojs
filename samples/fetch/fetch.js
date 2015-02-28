9886d5bc30562c04b78a378522077e2c3f4098fd
Chris Toshok <toshok@gmail.com>
2015-02-28 11:11:17 -0800

    add a timer

--- samples/fetch/fetch.js
+++ samples/fetch/fetch.js
@@ -22,9 +22,12 @@ if (process.argv.length < 2) {
 }
 let url_to_fetch = process.argv[1];
 
+console.time("fetch url");
 fetchUrl(url_to_fetch).then((data) => {
   console.log(data);
+  console.timeEnd("fetch url");
 }).catch((err) => {
   console.log(`error fetching ${url_to_fetch}`);
   console.log(err);
+  console.timeEnd("fetch url");
 });
