ae9385a1f5a33c3d85efdb07fcb0615313b334e3
Chris Toshok <toshok@gmail.com>
2015-02-28 11:07:57 -0800

    add a simple fetch-url sample

---
+++ samples/fetch/fetch.js
@@ -0,0 +1,30 @@
+function fetchUrl (url) {
+  let promise = new Promise((resolve, reject) => {
+    let client = new XMLHttpRequest();
+    client.open("GET", url);
+    client.onreadystatechange = () => {
+      if (client.readyState !== 4)
+        return;
+      if (client.status === 200)
+        resolve(client.responseText);
+      else
+        reject(client);
+    };
+    client.send();
+  });
+
+  return promise;
+}
+
+if (process.argv.length < 2) {
+  console.log(`usage:  ${process.argv[0]} $url`);
+  process.exit(-1);
+}
+let url_to_fetch = process.argv[1];
+
+fetchUrl(url_to_fetch).then((data) => {
+  console.log(data);
+}).catch((err) => {
+  console.log(`error fetching ${url_to_fetch}`);
+  console.log(err);
+});
