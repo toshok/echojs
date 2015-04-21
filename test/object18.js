af648da3f5be81926acb3210508b64737c5db878
Chris Toshok <toshok@gmail.com>
2015-04-21 08:13:07 -0700

    add (failing) test for computed accessors in object expressions from kangax's table

---
+++ test/object18.js
@@ -0,0 +1,12 @@
+function test() {
+        var x = 'y',
+            valueSet,
+            obj = {
+              get [x] () { return 1 },
+              set [x] (value) { valueSet = value }
+            };
+        obj.y = 'foo';
+        return obj.y === 1 && valueSet === 'foo';
+}
+
+console.log(test());
