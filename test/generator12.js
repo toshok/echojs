2690f6b6e19244e2a12b4cdad798c32036ff5629
Chris Toshok <toshok@gmail.com>
2015-04-21 08:08:45 -0700

    correct mis-c&p.  generator12 works

--- test/generator12.js
+++ test/generator12.js
@@ -1,12 +1,12 @@
 // "yield *, astral plane strings" from kangax
 
 var iterator = (function * generator() {
-  yield * 'u20BB7u20BB6;';
+  yield * "𠮷𠮶";
 }());
 var item = iterator.next();
-var passed = item.value === 'u20BB7' && item.done === false;
+var passed = item.value === "𠮷" && item.done === false;
 item = iterator.next();
-passed    &= item.value === 'u20BB6' && item.done === false;
+passed    &= item.value === "𠮶" && item.done === false;
 item = iterator.next();
 passed    &= item.value === undefined && item.done === true;
 console.log(passed);
