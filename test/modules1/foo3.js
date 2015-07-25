34236f5717b3e8f0cd9171cf283a07ee8652e399
Chris Toshok <toshok@gmail.com>
2015-07-24 17:40:56 -0700

    port to new version of esprima

--- test/modules1/foo3.js
+++ test/modules1/foo3.js
@@ -3,7 +3,7 @@ import methodInFoo3_1 from "foo3.1"
 import methodInFoo3_2 from "foo3.2"
 import methodInFoo3_3 from "foo3.3"
 
-export default = {
+export default {
   methodInFoo3_1: methodInFoo3_1,
   methodInFoo3_2: methodInFoo3_2,
   methodInFoo3_3: methodInFoo3_3
