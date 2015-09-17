bc878f0feb7832ae279079c9eb48165d545f982f
Chris Toshok <toshok@gmail.com>
2015-09-17 10:13:24 -0700

    clean up the tester script slightly, both in terms of code and output.

--- test/tester.js
+++ test/tester.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var os = require('os');
-var fs = require('fs');
-var glob = require('glob');
-var child_process = require('child_process');
-var spawn = child_process.spawn;
-var exec = child_process.exec;
-var colors = require('colors/safe');
-var temp = require('temp');
+var path = require('path'),
+    os = require('os'),
+    fs = require('fs'),
+    glob = require('glob'),
+    child_process = require('child_process'),
+    spawn = child_process.spawn,
+    exec = child_process.exec,
+    colors = require('colors/safe'),
+    temp = require('temp');
 
 // maps from test_name -> properties as defined in the test file
-var skip_ifs = Object.create(null);     // `// skip-if: ...` an expression, evaled.  if true, ignore the test
-var xfails = Object.create(null);       // `// xfail: ...`   test is expected to fail.  ... is the reason
+var skip_ifs   = Object.create(null);   // `// skip-if: ...` an expression, evaled.  if true, ignore the test
+var xfails     = Object.create(null);   // `// xfail: ...`   test is expected to fail.  ... is the reason
 var generators = Object.create(null);   // `// generator: ...` ... is the executable used to generate expected output
 
-var expected_names = Object.create(null);
+var expected_names   = Object.create(null);
 var expected_stdouts = Object.create(null);
-var stdouts = Object.create(null);
+var stdouts          = Object.create(null);
 
 var failed_tests = [];
 
@@ -35,10 +35,17 @@ var stage_to_run = 0;
 
 var test_threads = 4;
 
-var fail_str = colors.red.bold("FAIL");
-var xfail_str = colors.yellow("xfail");
-var xpass_str = colors.red.bold("ERROR");
-var pass_str = colors.green("pass");
+var result_types = {
+    fail:  { str: "FAIL",  colorizer: colors.red.bold },
+    xfail: { str: "xfail", colorizer: colors.yellow },
+    xpass: { str: "ERROR", colorizer: colors.red.bold },
+    pass:  { str: "pass",  colorizer: colors.green }
+};
+
+var fail_str = 'fail';
+var xfail_str = 'xfail';
+var xpass_str = 'xpass';
+var pass_str  = 'pass';
 
 function timerStart() {
   return process.hrtime();
@@ -50,26 +57,53 @@ function getElapsed(start_time) {
     return elapsed_ms.toFixed(2); // 2 decimal places
 }
 
-function writeOutput(test_name, result_string, elapsed) {
-    var spaces_40 = Array(41).join(" ");
-    var elapsed_str = elapsed == null ? "" : (elapsed + " ms");
-    console.log(spaces_40.substr(0, 40 - test_name.length) + test_name + "  " + result_string + " " + elapsed_str);
+function makeJustifierColumn(columns, leftJustify) {
+    var spaces = Array(columns).join(" ");
+    return function(str, transformer) {
+	var padding = spaces.substr(0, columns - str.length);
+	if (transformer)
+	    str = transformer(str);
+	if (leftJustify)
+	    return str + padding;
+	else
+	    return padding + str;
+    };
+}
+
+function makeNoopColumn() {
+    return function (x) { return x; };
+}
+
+var testColumn      = makeJustifierColumn(40, false);
+var resultColumn    = makeJustifierColumn(5, true);    // maximum length of fail/xfail/xpass/pass
+var timeColumn      = makeJustifierColumn(11, false);  // enough to hold "XXXXX.XX ms".
+var errStringColumn = makeNoopColumn();
+
+function writeOutput(test_name, result_type, elapsed, err_string) {
+    var elapsed_str = elapsed == null ? "?" : elapsed;
+
+    console.log(
+	testColumn(test_name),
+	resultColumn(result_types[result_type].str, result_types[result_type].colorizer),
+	timeColumn(elapsed_str + " ms"),
+	errStringColumn(err_string ? err_string : "")
+    );
 }
 
 function testFailure(test_name, err_string, elapsed, additional) {
-    writeOutput(test_name, fail_str + "(" + err_string + ")", elapsed);
+    writeOutput(test_name, fail_str, elapsed, "(" + err_string + ")");
     console.log(additional);
     failed_tests.push(test_name);
 }
 
 function testUnexpectedPass(test_name, elapsed) {
-    writeOutput(test_name, xpass_str + "(unexpected pass)", elapsed);
+    writeOutput(test_name, xpass_str, elapsed, "(unexpected pass)");
     failed_tests.push(test_name);
 }
 
 function testFailed(test_name, err_string, elapsed, additional) {
     if (xfails[test_name]) {
-	writeOutput(test_name, xfail_str + "(" + xfails[test_name] + ")", elapsed);
+	writeOutput(test_name, xfail_str, elapsed, "(" + xfails[test_name] + ")");
     }
     else {
         testFailure(test_name, err_string, elapsed, additional);
@@ -100,6 +134,18 @@ function checkStdout(test_name, elapsed, cb) {
     }
 }
 
+function shouldGenerateExpectedOutput(test_file, expected_file) {
+    var test_stat = fs.statSync(test_file);
+    try {
+	var expected_stat = fs.statSync(expected_file);
+	return test_stat.mtime.getTime() > expected_stat.mtime.getTime();
+    }
+    catch (e) {
+	// XXX verify that e == ENOENT
+	return true;
+    }
+}
+
 function processOneTest(gen_expected, test, cb) {
     var test_name = path.basename(test);
 
@@ -115,22 +161,11 @@ function processOneTest(gen_expected, test, cb) {
     if (gen_expected) {
 	var expected_name = "./expected/" + test_name + ".expected-out";
 
-	var test_stat = fs.statSync(test);
-
-	var need_generate = false;
-
-	try {
-	    var expected_stat = fs.statSync(expected_name);
-	    need_generate = test_stat.mtime.getTime() > expected_stat.mtime.getTime();
-	}
-	catch (e) {
-	    // XXX verify that e == ENOENT
-	    need_generate = true;
-	}
+	var should_generate = shouldGenerateExpectedOutput(test, expected_name);
 
 	expected_names[test_name] = expected_name;
 	var generator = generators[test_name] || "node";
-	if (need_generate && generator !== "none") {
+	if (should_generate && generator !== "none") {
 	    console.log("generating expected output for " + test_name + " using " + generator);
 
 	    exec(generator + " " + test + " > " + expected_name, function (err, stdout) {
@@ -166,7 +201,7 @@ function processOneTest(gen_expected, test, cb) {
 		cexec.on("error", function(err) {
                     var elapsed = getElapsed(start);
 		    testFailed(test_name, err.toString(), elapsed);
-		    setTimeout(cb, 0);
+		    cb();
 		});
 		cexec.stdout.on("data", function(msg) {
 		    test_stdout += msg;
@@ -178,7 +213,7 @@ function processOneTest(gen_expected, test, cb) {
 	    ccomp.on("error", function(err) {
                 var elapsed = getElapsed(start);
 		testFailed(test_name, err.toString(), elapsed);
-		setTimeout(cb, 0);
+		cb();
 		return;
 	    });
 	}
@@ -256,21 +291,19 @@ var args = process.argv.slice(2);
 var test_to_run = null;
 
 if (args[0] == '-s') {
-    if (args.length < 2)
+    args.shift();
+    if (args.length < 1)
 	throw new Error("-s requires an argument between 0 and 2");
-    stage_to_run = parseInt(args[1]);
+    stage_to_run = parseInt(args.shift());
     if (stage_to_run < 0 && stage_to_run > 2)
 	throw new Error("-s requires an argument between 0 and 2");
-    args.shift();
-    args.shift();
 }
 if (args[0] == '-t') {
-    if (args.length < 2)
+    args.shift();
+    if (args.length < 1)
 	throw new Error("-t requires an argument (the test file to run)");
-    test_to_run = args[1];
+    test_to_run = args.shift();
     test_threads = 1; // XXX workaround for a bug, but we also only need 1 thread when we're running 1 test
-    args.shift();
-    args.shift();
 }
 
 function runTests(tests) {
@@ -283,14 +316,18 @@ function runTests(tests) {
 	
     processTests(true, tests, function() {
 	processTests(false, tests, function() {
-	    if (failed_tests.length > 0) {
-                console.log("failed tests:");
+	    var run_failed = failed_tests.length > 0;
+	    if (run_failed > 0) {
+		console.log();
+                console.log(testColumn("failed tests"));
+		console.log(testColumn("============"));
                 failed_tests.forEach(function (t) {
-		    console.log(" " + t);
+		    console.log(testColumn(t));
                 });
 	    }
-	    console.log("done");
-	    process.exit(failed_tests.length > 0 ? 1 : 0);
+	    console.log(testColumn(" "),
+			resultColumn("done", result_types[run_failed ? "fail" : "pass"].colorizer));
+	    process.exit(run_failed ? 1 : 0);
 	});
     });
 }
