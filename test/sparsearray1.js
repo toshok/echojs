3c843857b2aa344242a5013ab6ed8cfd65a3c1e3
Chris Toshok <toshok@gmail.com>
2016-04-11 05:52:41 -0700

    typo

--- test/sparsearray1.js
+++ test/sparsearray1.js
@@ -1,4 +1,4 @@
-// xfail: sparse array support is pretty week and full of NOT_IMPLEMENTED's
+// xfail: sparse array support is pretty weak and full of NOT_IMPLEMENTED's
 var arr = new Array(1000000000);
 arr[0] = "Hello World";
 console.log (arr[0]);
