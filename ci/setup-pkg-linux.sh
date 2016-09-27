16f558a1b2fa10a881e0625c905069dc66b55484
Chris Toshok <toshok@gmail.com>
2016-09-27 10:01:25 -0700

    update libunwind dep

--- ci/setup-pkg-linux.sh
+++ ci/setup-pkg-linux.sh
@@ -3,7 +3,7 @@ wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-add-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main'
 sudo apt-get update
 sudo apt-get install llvm-3.8-dev clang-3.8 clang++-3.8
-sudo apt-get install libunwind7-dev make autoconf automake libedit-dev zlib1g-dev cmake
+sudo apt-get install libunwind8-dev make autoconf automake libedit-dev zlib1g-dev cmake
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 10
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 10
 #sudo update-alternatives --install /usr/bin/gcc      gcc /usr/bin/gcc-4.9 10
