de45a85395d2c2056f8dad75a1fd2c06bb05a1e3
Chris Toshok <toshok@gmail.com>
2016-09-27 23:43:43 -0700

    update-alternatives

--- ci/install-llvm-linux.sh
+++ ci/install-llvm-linux.sh
@@ -1,12 +1,12 @@
 set -ex
 sudo apt-get install llvm-3.8-dev clang-3.8
-sudo update-alternatives --install /usr/bin/llc      llc /usr/bin/llc-3.8 10
-sudo update-alternatives --install /usr/bin/opt      opt /usr/bin/opt-3.8 10
-sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-3.8 10
-sudo update-alternatives --install /usr/bin/llvm-dis llvm-dis /usr/bin/llvm-dis-3.8 10
-sudo update-alternatives --install /usr/bin/llvm-as  llvm-as  /usr/bin/llvm-as-3.8 10
-sudo update-alternatives --install /usr/bin/clang    clang    /usr/bin/clang-3.8 10
-sudo update-alternatives --install /usr/bin/clang++  clang++  /usr/bin/clang++-3.8 10
+sudo update-alternatives --install /usr/bin/llc      llc /usr/bin/llc-3.8 10 --force
+sudo update-alternatives --install /usr/bin/opt      opt /usr/bin/opt-3.8 10 --force
+sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-3.8 10 --force
+sudo update-alternatives --install /usr/bin/llvm-dis llvm-dis /usr/bin/llvm-dis-3.8 10 --force
+sudo update-alternatives --install /usr/bin/llvm-as  llvm-as  /usr/bin/llvm-as-3.8 10 --force
+sudo update-alternatives --install /usr/bin/clang    clang    /usr/bin/clang-3.8 10 --force
+sudo update-alternatives --install /usr/bin/clang++  clang++  /usr/bin/clang++-3.8 10 --force
 llvm-config --version
 update-alternatives --list llvm-config
 type -all llvm-config
