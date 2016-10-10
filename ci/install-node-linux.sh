791b89f2dacb76c738b55e73bfd084d8dc1c5334
Chris Toshok <toshok@gmail.com>
2016-10-10 22:56:47 +0000

    linux fixes

--- ci/install-node-linux.sh
+++ ci/install-node-linux.sh
@@ -2,4 +2,5 @@ set -ex
 
 NPM=`which npm`
 
+sudo apt-get install nodejs build-essential
 sudo $NPM install -g node-gyp
--- ci/setup-pkg-linux.sh
+++ ci/setup-pkg-linux.sh
@@ -1,5 +1,6 @@
 set -ex
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-add-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main'
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install libunwind8-dev make autoconf automake libedit-dev zlib1g-dev cmake
+sudo apt-get install libunwind8-dev make autoconf automake libedit-dev zlib1g-dev cmake libuv-dev
